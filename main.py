3ad42af81c7e65413ae6f9b6e190619a36a1c2c5
sekkragen33 <98112010+sekkragen33@users.noreply.github.com>
2022-01-20 19:32:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,8 @@ class Start(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = WIDTH * 0.33
         self.rect.y = HEIGHT * 0.375
+        
+    
 
 
 if __name__ == '__main__':
@@ -97,3 +99,28 @@ if __name__ == '__main__':
             mouse_sprite.update()
         pygame.display.flip()
     pygame.quit()
+  class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
+        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.image = Surface((WIDTH, HEIGHT))
+        self.image.fill(Color(COLOR))
+        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
+
+    def update(self, left, right):
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+
+    def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+  
